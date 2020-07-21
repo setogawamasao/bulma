e223f826f2d38a532d069ce5b156f4a29a3d40a7
masaosetogawa <ms.job.hunting@gmail.com>
2020-07-21 23:27:34 +0900

    Add view port

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bulma/css/bulma.css";
-import App from "./App";
+
+const App = () => {
+  return (
+    <div className="field">
+      <label className="label">Label</label>
+      <div className="control">
+        <input className="input" type="text" placeholder="Text input" />
+      </div>
+      <p className="help">This is a help text</p>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
